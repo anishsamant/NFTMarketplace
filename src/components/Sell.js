9cd51bbe4ab69a3bbcb2a335ad36a601b529a59e
Yash Bhalerao <bhaleraoyash6@gmail.com>
2022-12-22 06:25:53 -0500

    Cards added

--- src/components/Sell.js
+++ src/components/Sell.js
@@ -1,14 +1,39 @@
 import React, { useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 
+function sellNFT(){
+	console.log('button clicked');
+}
+
 function Popup(props) {
     const [inp, setInp] = useState('');
+	let index = 0;
 	const myBirdz = []
 	for(let i = 0; i < props.kryptoBirdz.length; i++) {
 		if(props.kryptoBirdz[i].owner == props.account){
-			console.log('true');
+			myBirdz[index++] = props.kryptoBirdz[i];
 		}
 	}
+	const listItems = this.state.kryptoBirdz.map((kbird, index) => 
+		<MDBCard key={kbird.kryptoBird} className='token img' style={{maxWidth: '25rem'}}>
+			<MDBCardImage 
+				src={kbird.kryptoBird}
+				alt={kbird.kryptoBird}
+				position='top'
+				height= '250px'
+			/>
+			<MDBCardBody>
+				<MDBCardTitle className="projects-card-title">KryptoBirdz</MDBCardTitle>
+				<MDBCardText className="projects-card-text">
+					<span>Owner: <span style={{color: '#0608A3', fontSize: '18px'}}>{kbird.owner}</span></span>
+				</MDBCardText>
+			</MDBCardBody>
+			<MDBCardFooter className="projects-card-footer">
+				<button onClick={() => sellNFT()}>Sell</button>
+			</MDBCardFooter>
+		</MDBCard>
+	);
+
     return (
       <Modal
         {...props}
@@ -27,6 +52,9 @@ function Popup(props) {
               <Form.Label>To Address</Form.Label>
             </Form.Group>
           </Form> */}
+		<div className="cards-container-style row">  
+			{listItems}
+		</div>
         </Modal.Body>
         <Modal.Footer>
             {/* <Button onClick={() => transfer(props.contract, props.from, inp, props.tokenid, props.context)}>Send</Button> */}

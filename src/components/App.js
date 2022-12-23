49a6a5fb31af6ebef04e102d0389272cc76f762c
Yash Bhalerao <bhaleraoyash6@gmail.com>
2022-12-23 14:58:12 -0500

    user added

--- src/components/App.js
+++ src/components/App.js
@@ -581,7 +581,7 @@ class App extends Component {
                         </ul>
                         <ul className='navbar-nav px-3'>
                             <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
-                                <button onClick={signOut}>Sign Out</button>
+                                <button onClick={signOut}>Sign Out : {user}</button>
                             </li>
                         </ul>
                     </nav>

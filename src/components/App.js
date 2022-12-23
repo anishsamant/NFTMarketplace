53bd8834a62d54ca3b51ee4d7f9b16f6b4a608ec
Yash Bhalerao <bhaleraoyash6@gmail.com>
2022-12-23 12:55:12 -0500

    npm install run

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ import {Amplify} from 'aws-amplify';
 import { withAuthenticator} from '@aws-amplify/ui-react';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import awsExports from 'aws-exports';
+import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 // AWS HTTP

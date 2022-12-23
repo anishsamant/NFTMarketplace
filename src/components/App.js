398dd4142e8a2510b75799c78d2e26e353c82ea4
Yash Bhalerao <bhaleraoyash6@gmail.com>
2022-12-23 14:39:16 -0500

    final

--- src/components/App.js
+++ src/components/App.js
@@ -577,7 +577,6 @@ class App extends Component {
                                     {this.state.account}
                                 </small>
                             </li>
-                            <button onClick={signOut}>Sign Out</button>
                         </ul>
                         <ul className='navbar-nav px-3'>
                             <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
